8292100b42ce9b7c6659bbb28ed84956eb4f1dab
Cody Benner <strykstaguy@gmail.com>
2021-02-06 21:52:02 -0600

    fixing clear filter

--- resources/views/bootstrap-4/includes/filter.blade.php
+++ resources/views/bootstrap-4/includes/filter.blade.php
@@ -1,6 +1,6 @@
 @if (count($filtersViews))
     <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-filter mr-1"></i>
             Filters {{ count($filters) ? '(' . count($filters) . ')' : '' }}
         </button>
@@ -17,10 +17,8 @@
 
                 @if ($clearFilterButton && count($filters) > 0)
                     <div class="px-4 py-2 bg-gray-100 text-right flex justify-end">
-                        <button type="button" wire:click="clearFilters" onclick="document.querySelector('#filter-form').reset()" class="btn btn-link">
-                            <span class="mr-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0H24V24H0z"/><path d="M6.929.515L21.07 14.657l-1.414 1.414-3.823-3.822L15 13.5V22H9v-8.5L4 6H3V4h4.585l-2.07-2.071L6.929.515zM9.585 6H6.404L11 12.894V20h2v-7.106l1.392-2.087L9.585 6zM21 4v2h-1l-1.915 2.872-1.442-1.443L17.596 6h-2.383l-2-2H21z"/></svg>
-                            </span>
+                        <button type="button" wire:click="clearFilters" onclick="document.querySelector('#filter-form').reset()" class="btn btn-secondary">
+                            <i class="fas fa-times mr-1"></i>
                             Clear filters
                         </button>
                     </div>
