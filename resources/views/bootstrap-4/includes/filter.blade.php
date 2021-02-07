16d48bb9068ba1a47890657e0bcdbdb7c275605f
Cody Benner <strykstaguy@gmail.com>
2021-02-06 21:40:19 -0600

    filter button fix

--- resources/views/bootstrap-4/includes/filter.blade.php
+++ resources/views/bootstrap-4/includes/filter.blade.php
@@ -1,9 +1,7 @@
 @if (count($filtersViews))
     <div class="dropdown">
         <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <span class="mr-1">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12" height="12"><path fill="none" d="M0 0H24V24H0z"/><path d="M21 4v2h-1l-5 7.5V22H9v-8.5L4 6H3V4h18zM6.404 6L11 12.894V20h2v-7.106L17.596 6H6.404z"/></svg>
-            </span>
+            <i class="fas fa-filter mr-1"></i>
             Filters {{ count($filters) ? '(' . count($filters) . ')' : '' }}
         </button>
         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
